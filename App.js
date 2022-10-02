651d4c95c3b873cd1bac8a2c3524f702b83351b5
Rosenornen <70527031+Rosenornen@users.noreply.github.com>
2022-10-02 02:09:44 +0200

    s

--- App.js
+++ App.js
@@ -1,42 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './screens/homeScreen';
-import SettingsScreen from './screens/settingsScreen';
-import MapScreen from './screens/mapScreen';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import Ionicons from "react-native-vector-icons/Ionicons";
 import { NavigationContainer } from '@react-navigation/native';
 
-const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
+import OnboardingScreen from './screens/onboardingScreen';
+import LoginScreen from './screens/loginScreen';
 
-export default function App() {
-  const StackNavigation = () => {
-    return (
-     <Stack.Navigator>
-        <Stack.Screen options={{headerShown: false}} name="Home" component={HomeScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen}/>
-        <Stack.Screen name="Map" component={MapScreen}/>
-      </Stack.Navigator>
-      );}
-  
+const AppStack = createStackNavigator();
+
+const App = () => {
   return (
     <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name = {'Home'} component = {StackNavigation} options = {{tabBarIcon: () => ( <Ionicons name='home' size={20}/>)}}/>
-          <Tab.Screen name = {'Settings'} component = {StackNavigation} options = {{tabBarIcon: () => ( <Ionicons name='settings' size={20}/>)}}/>
-          <Tab.Screen name = {'Map'} component = {StackNavigation} options = {{tabBarIcon: () => ( <Ionicons name='map' size={20}/>)}}/>
-        </Tab.Navigator>
+      <AppStack.Navigator
+        headerMode = "none"
+      >
+        <AppStack.Screen name='Onboarding' component={OnboardingScreen}/>
+        <AppStack.Screen name='Login' component={LoginScreen}/>
+      </AppStack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
